9ef5d0941bb374a7403caf5f7803a35f3877347a
Nabin Thapa <nabin.thapa@dakshyanepal.com>
2021-06-29 04:55:06 +0545

    complete product navigation

--- components/Navigations/ProductNav/ProductNav.jsx
+++ components/Navigations/ProductNav/ProductNav.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 const ProductNav = ({ items }) => {
-  const [active, setActive] = useState("");
+  const [active, setActive] = useState();
   return (
-    <div className="bg-[#EBEBEB] rounded-full py-3 px-2 flex items-center justify-evenly w-[564px]">
+    <div className="bg-[#EBEBEB] rounded-full py-3 px-2 flex items-center justify-evenly w-[564px] bg-opacity-[59%]">
       {items.map((item, index) => (
         <div
           className={`font-primary text-base text-gray-900 capitalize hover:bg-blue-500 rounded-full py-2 px-5 hover:text-white transition-all ease-in-out duration-300 ${
