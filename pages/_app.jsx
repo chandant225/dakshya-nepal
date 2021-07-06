e7ec5b08fd64d23f06c11adb2d8117d3413d57b3
dakshya-mahesh <mahesh.aryal@dakshyanepal.com>
2021-07-06 16:55:33 +0545

    chat

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,11 +9,18 @@ import "../styles/skeleton.css";
 import "../styles/custom-text-slick.css";
 import "../styles/fonts.css";
 import "aos/dist/aos.css";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function MyApp({ Component, pageProps }) {
   return (
     <RootLayout>
       <Component {...pageProps} />
+      <MessengerCustomerChat
+        pageId="1454212978176971"
+        appId="779641199345768"
+        themeColor="#ffb001"
+      />
+      ,
     </RootLayout>
   );
 }
