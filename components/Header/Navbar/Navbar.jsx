324feedd95cddbd1b845f81c42d4197552d535c4
dakshya-chandan <chandan.thakur@dakshyanepal.com>
2021-10-01 11:50:35 +0545

    adding-facebook-metatags

--- components/Header/Navbar/Navbar.jsx
+++ components/Header/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { PrimaryButton, SecondaryButton } from "../../Buttons";
 import styled from "styled-components";
 import { ArrowNarrowLeftIcon, MenuIcon, XIcon } from "@heroicons/react/solid";
+import Head from "next/head";
 
 // local
 import dakshyaLogo from "../../../public/icons/dakshya_logo.svg";
@@ -27,155 +28,149 @@ const Navbar = () => {
   };
 
   return (
-    <div
-      className={`py-6 ${
-        router.pathname !== "/" && "bg-white shadow sticky top-0 z-50"
-      } ${
-        router.pathname === "/" && navActive
-          ? "sticky top-0 bg-white shadow z-50"
-          : ""
-      }`}
-    >
-      <div className="container mx-auto  justify-between items-center flex">
-        <Link href="/">
-          <Image
-            src={dakshyaLogo}
-            alt="dakshya nepal"
-            className="w-[180px] h-10 cursor-pointer"
+    <>
+      <Head>
+        <meta property="fb:pages" content="115400240252220" />
+      </Head>
+      <div
+        className={`py-6 ${router.pathname !== "/" && "bg-white shadow sticky top-0 z-50"
+          } ${router.pathname === "/" && navActive
+            ? "sticky top-0 bg-white shadow z-50"
+            : ""
+          }`}
+      >
+        <div className="container mx-auto  justify-between items-center flex">
+          <Link href="/">
+            <Image
+              src={dakshyaLogo}
+              alt="dakshya nepal"
+              className="w-[180px] h-10 cursor-pointer"
+            />
+          </Link>
+          <MenuIcon
+            style={{ zIndex: "999" }}
+            onClick={() => {
+              OpenNav();
+            }}
+            className="text-black w-8 z-50 h-8 lg:hidden"
           />
-        </Link>
-        <MenuIcon
-          style={{ zIndex: "999" }}
-          onClick={() => {
-            OpenNav();
-          }}
-          className="text-black w-8 z-50 h-8 lg:hidden"
-        />
-        <ul className="items-center space-x-[32px] hidden lg:flex">
-          <li
-            className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/products") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/products">Products</Link>
-          </li>
-          <li
-            className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/blogs") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/blogs">Blogs</Link>
-          </li>
-          <li
-            className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/about") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/about">About</Link>
-          </li>
-          <li
-            className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/careers") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/careers">Careers</Link>
-          </li>
-          <button className="font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300">
-            क
-          </button>
-          <a
-            href="https://shop.dakshyanepal.com"
-            target="_blank"
-            className="inline-block py-3 px-5 text-center bg-black text-white font-semibold text-base rounded-full shadow border-none hover:border-4 border-black hover:text-black hover:bg-gray-50 transition-all ease-linear duration-500"
-          >
-            Shop Now
-          </a>
-        </ul>
-      </div>
-      <div id="my_Sidebar" className="Sidebar">
-        <span
-          onClick={() => {
-            CloseNav();
-          }}
-        >
-          <XIcon className="text-white w-10 float-right mr-2 mt-2" />
-        </span>
-        <ul className="mt-14 pl-4 space-y-3 md:space-y-0">
-          <li
-            onClick={() => {
-              CloseNav();
-            }}
-            className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname === "/" && "text-yellow-500"
-            }`}
-          >
-            <Link href="/">Home</Link>
-          </li>
-          <li
-            onClick={() => {
-              CloseNav();
-            }}
-            className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/products") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/products">Products</Link>
-          </li>
-          <li
-            onClick={() => {
-              CloseNav();
-            }}
-            className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/blogs") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/blogs">Blogs</Link>
-          </li>
-          <li
-            onClick={() => {
-              CloseNav();
-            }}
-            className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/about") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/about">About</Link>
-          </li>
-          <li
-            onClick={() => {
-              CloseNav();
-            }}
-            className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${
-              router.pathname.includes("/careers") && "text-yellow-500"
-            }`}
-          >
-            <Link href="/careers">Careers</Link>
-          </li>
-          <button
-            onClick={() => {
-              CloseNav();
-            }}
-            className="font-medium py-2 text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300"
-          >
-            क
-          </button>
-          <li
-            onClick={() => {
-              CloseNav();
-            }}
-            className="font-medium py-2 text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300"
-          >
+          <ul className="items-center space-x-[32px] hidden lg:flex">
+            <li
+              className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/products") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/products">Products</Link>
+            </li>
+            <li
+              className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/blogs") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/blogs">Blogs</Link>
+            </li>
+            <li
+              className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/about") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/about">About</Link>
+            </li>
+            <li
+              className={`font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/careers") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/careers">Careers</Link>
+            </li>
+            <button className="font-medium text-base text-black font-secondary hover:text-gray-500 transition-all ease-in-out duration-300">
+              क
+            </button>
             <a
               href="https://shop.dakshyanepal.com"
               target="_blank"
-              className="outline-none"
+              className="inline-block py-3 px-5 text-center bg-black text-white font-semibold text-base rounded-full shadow border-none hover:border-4 border-black hover:text-black hover:bg-gray-50 transition-all ease-linear duration-500"
             >
               Shop Now
             </a>
-          </li>
-        </ul>
+          </ul>
+        </div>
+        <div id="my_Sidebar" className="Sidebar">
+          <span
+            onClick={() => {
+              CloseNav();
+            }}
+          >
+            <XIcon className="text-white w-10 float-right mr-2 mt-2" />
+          </span>
+          <ul className="mt-14 pl-4 space-y-3 md:space-y-0">
+            <li
+              onClick={() => {
+                CloseNav();
+              }}
+              className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname === "/" && "text-yellow-500"
+                }`}
+            >
+              <Link href="/">Home</Link>
+            </li>
+            <li
+              onClick={() => {
+                CloseNav();
+              }}
+              className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/products") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/products">Products</Link>
+            </li>
+            <li
+              onClick={() => {
+                CloseNav();
+              }}
+              className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/blogs") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/blogs">Blogs</Link>
+            </li>
+            <li
+              onClick={() => {
+                CloseNav();
+              }}
+              className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/about") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/about">About</Link>
+            </li>
+            <li
+              onClick={() => {
+                CloseNav();
+              }}
+              className={`font-medium text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300 ${router.pathname.includes("/careers") && "text-yellow-500"
+                }`}
+            >
+              <Link href="/careers">Careers</Link>
+            </li>
+            <button
+              onClick={() => {
+                CloseNav();
+              }}
+              className="font-medium py-2 text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300"
+            >
+              क
+            </button>
+            <li
+              onClick={() => {
+                CloseNav();
+              }}
+              className="font-medium py-2 text-2xl text-white font-secondary hover:text-gray-500 transition-all ease-in-out duration-300"
+            >
+              <a
+                href="https://shop.dakshyanepal.com"
+                target="_blank"
+                className="outline-none"
+              >
+                Shop Now
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
